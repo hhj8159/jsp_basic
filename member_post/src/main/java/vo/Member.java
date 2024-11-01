91bebef871ddd6f04358db8da367727b21a0c1c3
mawoo <hanjutun@gmail.com>
2024-11-01 18:36:02 +0900

    sevlet

---
+++ member_post/src/main/java/vo/Member.java
@@ -0,0 +1,17 @@
+package vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	private String id;
+	private String pw;
+	private String name;
+	private String email;
+	private String roadAddr;
+	private String detailAddr;
+	private Date regdate;
+}
+
